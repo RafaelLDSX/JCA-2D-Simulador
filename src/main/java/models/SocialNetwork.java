c6c3fca2b88ba46da6cef487ecc4f1ec4d9ce1ed
Rafael Xavier <rafael_ldsx@hotmail.com>
2025-03-17 23:45:37 -0300

    inicio do autômato SocialNetwork
    definições de variáveis iniciais

---
+++ src/main/java/models/SocialNetwork.java
@@ -0,0 +1,29 @@
+package models;
+
+public class SocialNetwork extends CellularAutomataModel{
+
+	private double alpha;
+	private double averageAlpha;
+	private double gamma;
+	private double averageGamma;
+	private double epsilon;
+	private double averageEpsilon;
+	
+	private double alphaRumorMI;
+	private double alphaRumorSIG;
+	private double gammaRumorMI;
+	private double gammaRumorSIG;
+	
+	private double maxOutDegree;
+	private double averageOutDegree;
+	private double maxInDegree;
+	private double averageInDegree;
+	
+	
+	public SocialNetwork() {
+		
+		this.maxCellStates = 5;
+		
+	}
+	
+}
