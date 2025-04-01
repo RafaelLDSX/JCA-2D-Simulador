b38d2f14ab828f0321b29f0db8717099768a0212
Rafael Xavier <rafael_ldsx@hotmail.com>
2025-04-01 00:29:59 -0300

    função de carregar configuração de arquivo json

--- src/main/java/models/socialNetwork/SocialNetwork.java
+++ src/main/java/models/socialNetwork/SocialNetwork.java
@@ -1,5 +1,15 @@
 package models.socialNetwork;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import models.CellularAutomataModel;
 
 public class SocialNetwork extends CellularAutomataModel{
@@ -21,11 +31,54 @@ public class SocialNetwork extends CellularAutomataModel{
 	private double maxInDegree;
 	private double averageInDegree;
 	
+	private double infected;
+	private String infectedOrder;
+	private int timesteps;
+	
 	
 	public SocialNetwork() {
 		
 		this.maxCellStates = 5;
 		
+	}
+	
+	@Override
+	public void initialCondition() {
+		System.out.println("initial condition");
+		this.state = CLASS_STATE_INITIAL_CONDITION;
+		
+		
+	}
+	
+	public void loadConfig(String fileName) {
+		 JSONParser parser = new JSONParser();
+		 
+		 try {
+			 Object obj = parser.parse(new FileReader(fileName));
+			 JSONObject globalParams = (JSONObject) obj;
+			 JSONObject json = (JSONObject) globalParams.get("global-params");
+			 
+			 this.width = ((Long) json.get("cell-x")).intValue();
+			 this.height = ((Long) json.get("cell-y")).intValue();
+			 this.infected = (double) json.get("infected");
+			 this.infectedOrder = (String) json.get("infected_init_order");
+			 this.timesteps = ((Long) json.get("timesteps")).intValue();
+			 this.alphaRumorMI = (double) json.get("alpha-rumor-mi");
+			 this.alphaRumorSIG = (double) json.get("alpha-rumor-sig");
+			 this.gammaRumorMI = (double) json.get("gamma-rumor-mi");
+			 this.gammaRumorSIG = (double) json.get("gamma-rumor-sig");
+			 
+			 
+		 } catch (FileNotFoundException e) {
+			 //melhorar catch
+			 e.printStackTrace();
+		 } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
