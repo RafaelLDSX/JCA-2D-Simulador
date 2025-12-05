3e0388ab742f875d817568a889236f71cb9e3018
Rafael Xavier <rafael_ldsx@hotmail.com>
2025-12-05 16:38:57 -0300

    buildAlpha, gamma e epsilon, além de função exec e update

--- src/main/java/models/socialNetwork/SocialNetwork.java
+++ src/main/java/models/socialNetwork/SocialNetwork.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
@@ -24,6 +25,7 @@ import models.CellularAutomataModel;
 
 public class SocialNetwork extends CellularAutomataModel{
 
+	private static double ERROR = 1E-20;
 	private double alpha;
 	private double averageAlpha;
 	private double gamma;
@@ -46,6 +48,10 @@ public class SocialNetwork extends CellularAutomataModel{
 	private String infectedOrder;
 	private int timesteps;
 	
+	private Random generator = new Random(System.nanoTime());
+	
+	private Graph<Agent, DefaultEdge> graph;
+	
 	private List<Agent> agents;
 	
 	
@@ -67,7 +73,7 @@ public class SocialNetwork extends CellularAutomataModel{
         normalSample = ThreadLocalRandom.current().nextGaussian();
         double gammaRumor = Math.exp(this.gammaRumorMI + this.gammaRumorSIG * normalSample);
         
-        Graph<Agent, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        this.graph = new DefaultDirectedGraph<>(DefaultEdge.class);
         int numberOfVertices;
         
         try (BufferedReader reader = new BufferedReader(new FileReader("BA-N-10-P-1-C-6.net"))) {
@@ -82,7 +88,7 @@ public class SocialNetwork extends CellularAutomataModel{
                     	Agent x = new Agent();
                     	x.setStates(State.SUSCEPTIBLE);
                     	this.agents.add(x);
-                    	graph.addVertex(x);
+                    	this.graph.addVertex(x);
                     }
                 }
             }
@@ -93,28 +99,28 @@ public class SocialNetwork extends CellularAutomataModel{
             		String[] indexes = line.split("\\s+");
             		auxAgent1 = this.agents.get(Integer.parseInt(indexes[0]) - 1);
             		auxAgent2 = this.agents.get(Integer.parseInt(indexes[1]) - 1);
-            		graph.addEdge(auxAgent1, auxAgent2);		
+            		this.graph.addEdge(auxAgent1, auxAgent2);		
             		
             	}
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        this.alpha = this.averageAlpha = this.gamma = this.averageGamma = this.epsilon = this.averageEpsilon = 0;
+        Agent maxInDegreeAgent = agents.stream().max(Comparator.comparing(x -> this.graph.inDegreeOf(x))).get();
+        Agent maxOutDegreeAgent = agents.stream().max(Comparator.comparing(x -> this.graph.outDegreeOf(x))).get();
+        this.maxInDegree = this.graph.inDegreeOf(maxInDegreeAgent);
+        this.maxOutDegree = this.graph.outDegreeOf(maxOutDegreeAgent);
         
-        Agent maxInDegreeAgent = agents.stream().max(Comparator.comparing(x -> graph.inDegreeOf(x))).get();
-        Agent maxOutDegreeAgent = agents.stream().max(Comparator.comparing(x -> graph.outDegreeOf(x))).get();
-        this.maxInDegree = graph.inDegreeOf(maxInDegreeAgent);
-        this.maxOutDegree = graph.outDegreeOf(maxOutDegreeAgent);
-        
-        double totalInDegree = agents.stream().collect(Collectors.summingInt(x -> graph.inDegreeOf(x)));
-        double totalOutDegree = agents.stream().collect(Collectors.summingInt(x -> graph.outDegreeOf(x)));
+        double totalInDegree = agents.stream().collect(Collectors.summingInt(x -> this.graph.inDegreeOf(x)));
+        double totalOutDegree = agents.stream().collect(Collectors.summingInt(x -> this.graph.outDegreeOf(x)));
         this.averageInDegree = totalInDegree / agents.size();
         this.averageOutDegree = totalOutDegree / agents.size();
         
         double[] probabilities = new double[agents.size()];
         double add = 0;
         for(int i = 0; i < agents.size(); i++) {
-        	add += graph.inDegreeOf(agents.get(i)) / totalInDegree;
+        	add += this.graph.inDegreeOf(agents.get(i)) / totalInDegree;
         	probabilities[i] = add;
         }
         
@@ -135,7 +141,7 @@ public class SocialNetwork extends CellularAutomataModel{
         		boolean selected = false;
         		
         		do {
-        			double p = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
+        			double p = generator.nextDouble();
             		add = probabilities[0];
             		int j = 1;
             		while (p > add) {
@@ -154,15 +160,34 @@ public class SocialNetwork extends CellularAutomataModel{
         }  
 	}
 	
+	public void statistic(int i) {}
+	
 	public void exec() {
+		int mark = this.timeStep / 10;
 		
+		for (int i = 1; i <= this.timeStep; i++) {
+			this.update(i);
+			this.statistic(i);
+			if (i % mark == 0) {
+			}
+		}
 	}
 	
 	public void update(int t) {
+		for (int i = 0; i < this.agents.size(); i++) {
+			switch (this.agents.get(i).getState()) {
+				case SUSCEPTIBLE: this.susceptible2NS(i, t);
+				case EXPOSED: this.exposed2NS(i, t);
+				case INFECTED: this.infected2NS(i, t);
+			}	
+		}
 		
+		for (int i = 0; i < this.agents.size(); i++) {
+			this.agents.get(i).update();
+		}
 	}
 	
-	double buildEpsilon(long node, long iTime) {
+	double buildEpsilon(int node, int iTime) {
 	    double dTime = (double) iTime;
 	    double epsilon = 1.0 - Math.exp(-this.lambda * dTime);
 
@@ -174,6 +199,127 @@ public class SocialNetwork extends CellularAutomataModel{
 	    }
 
 	    return epsilon;
+	}
+	
+	double buildAlpha(int node, int iTime) {
+		double infected = 0.0;
+		double recovered = 0.0;
+		double alpha = 0.0;
+		
+		List<Agent> outAgents = this.agents.get(node).outAgents;
+		
+		for (Agent agent : outAgents) {
+			if (agent.state0 == State.INFECTED) {
+				infected += this.graph.inDegreeOf(agent);
+			} else if (agent.state0 == State.RECOVERED) {
+				recovered += this.graph.inDegreeOf(agent);
+			}
+		}
+		
+		double p1 = Math.exp(this.alphaRumorMI + this.alphaRumorSIG * generator.nextGaussian());
+
+		double t1 = (double) iTime;
+		
+		if (iTime > 0)
+			t1 /= p1;
+		else
+			t1 = ERROR / p1;
+		
+		alpha = (1.0 - Math.exp(-((infected / this.maxInDegree) * t1)));
+		
+		if (alpha < ERROR)
+			alpha = 0;
+		
+		return alpha;
+	}
+	
+	double buildGamma(int node, int iTime) {
+		double infected = 0.0;
+		double recovered = 0.0;
+		double gamma = 0.0;
+		
+		List<Agent> outAgents = this.agents.get(node).outAgents;
+		
+		for (Agent agent : outAgents) {
+			if (agent.state0 == State.RECOVERED) {
+				recovered += this.graph.inDegreeOf(agent);
+			}
+		}
+		
+		double p1 = Math.exp(this.gammaRumorMI + this.gammaRumorSIG * generator.nextGaussian());
+		
+		double t1 = (double) iTime;
+		
+		if (iTime > 0)
+			t1 /= p1;
+		else
+			t1 = ERROR / p1;
+		
+		gamma = (1.0 - Math.exp(-((infected / this.maxInDegree) * t1)));
+		
+		if (gamma < ERROR)
+			gamma = 1.0 -Math.exp(-(double) iTime);
+		
+		return gamma;
+	}
+	
+	void susceptible2NS(int node, int iTime) {
+		double epsilon = this.buildEpsilon(node, iTime);
+		double alpha = this.buildAlpha(node, iTime);
+		double prob = generator.nextDouble();
+		
+		this.averageAlpha += alpha;
+		this.alpha++;
+		
+		this.averageEpsilon += epsilon;
+		this.epsilon++;
+		
+		double b = alpha + epsilon * (1.0 - alpha);
+		
+		if (prob < alpha)
+			this.agents.get(node).setState(State.INFECTED);
+		else if (alpha <= prob && prob < b)
+			this.agents.get(node).setState(State.EXPOSED);
+		else
+			this.agents.get(node).setState(this.agents.get(node).getState());
+		
+	}
+	
+	void exposed2NS(int node, int iTime) {
+		double gamma = buildGamma(node, iTime);
+		double alpha = buildAlpha(node, iTime);
+		
+		//TODO if alpha < 0 exit(1)
+		
+		this.averageAlpha += alpha;
+		this.alpha++;
+		
+		this.averageGamma += gamma;
+		this.gamma++;
+		
+		double prob = generator.nextDouble();
+		double b = gamma + (alpha * (1.0 - gamma));
+		
+		if (prob < gamma)
+			this.agents.get(node).setState(State.RECOVERED);
+		else if (gamma <= prob && prob <= b) 
+			this.agents.get(node).setState(State.INFECTED);
+		else 
+			this.agents.get(node).setState(this.agents.get(node).getState());
+	}
+	
+	void infected2NS(int node, int iTime) {
+		double gamma = buildGamma(node, iTime);
+		
+		this.averageGamma += gamma;
+		this.gamma++;
+		
+		double prob = generator.nextDouble();
+		
+		if (prob < gamma)
+			this.agents.get(node).setState(State.RECOVERED);
+		else
+			this.agents.get(node).setState(this.agents.get(node).getState());
 	}
 	
 	public void loadConfig(String fileName) {
